e5575d8928c38f20a13578d84ff9f0b9046bbf33
armanruet <armanruet@gmail.com>
2026-04-26 13:00:20 +0000

    Fix error

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def remove_char_at(name, n):
-    print(" " + name[1:] if n == 0 else name[0:n]+" " + name[n+1:])
+def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return str
+    return str[:n] + str[n+1:]
