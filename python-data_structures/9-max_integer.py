c00541511b168209aff4008165637aef1207ba00
armanruet <armanruet@gmail.com>
2026-04-28 09:11:36 +0000

    Task 9 done

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-	max_value = max(my_list)
+    if len(my_list) < 1:
+        return None
+    else:
+        print("Max: {}".format(sorted(my_list)[-1]))
