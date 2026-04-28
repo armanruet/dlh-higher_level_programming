4f27e9c56a272b19059690f4c4529f588dccd8f6
armanruet <armanruet@gmail.com>
2026-04-28 08:55:15 +0000

    Task 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,5 +3,5 @@ def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
     else:
-        my_list.remove(my_list[idx-1])
+        my_list.remove(my_list[idx])
         return my_list
