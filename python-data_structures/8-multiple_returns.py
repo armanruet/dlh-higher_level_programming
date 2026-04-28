b679f7c4f3065aef42eea52ec7d9ee02be93cec7
armanruet <armanruet@gmail.com>
2026-04-28 09:30:54 +0000

    Task 8 added

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) < 1:
+        return (len(sentence), None)
+    else:
+        length, first = len(sentence), sentence[0]
+        return (length, first)
