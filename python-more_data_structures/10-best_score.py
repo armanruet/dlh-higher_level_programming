1718afca4a46c487d9a2b43e3df7d30a61f9a82e
armanruet <armanruet@gmail.com>
2026-04-30 13:17:42 +0000

    Task 10 is done

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        return list(dict(sorted(a_dictionary.items(), key=lambda x: x[1])).keys())[-1]
+        sorted_d = sorted(a_dictionary.items(), key=lambda x: x[1])
+        return list(dict(sorted_d).keys())[-1]
