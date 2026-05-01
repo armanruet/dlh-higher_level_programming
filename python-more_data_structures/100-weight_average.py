581cab33cf9cc829b50b9cba5ee448f687603014
armanruet <armanruet@gmail.com>
2026-05-01 09:43:34 +0000

    resolved issues

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-	total = 0
-	dev = 0
-	for i,j in my_list:
-		total += i * j
-		dev += j
-	return total / dev
+    total = 0
+    dev = 0
+    if not my_list:
+        return 0
+    for i, j in my_list:
+        total += i * j
+        dev += j
+    return total / dev
