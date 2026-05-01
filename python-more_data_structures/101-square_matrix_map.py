c4ec77f8905cb27a7c2398d91a7f0ebd7d316405
armanruet <armanruet@gmail.com>
2026-05-01 18:59:22 +0000

    extra task 101

--- python-more_data_structures/101-square_matrix_map.py
+++ python-more_data_structures/101-square_matrix_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-	sum = []
-	for i in matrix:
-		sum.append(list(map(lambda x: x ** 2, i)))
-	return sum
+    return list(map(lambda row: [x ** 2 for x in row], matrix))
