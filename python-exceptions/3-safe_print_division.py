e210e1d1fd2d421cad09661e70d851be4c828951
armanruet <armanruet@gmail.com>
2026-05-02 10:11:06 +0000

    Task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a/b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
+
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
