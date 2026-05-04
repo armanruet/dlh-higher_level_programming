f8b09df044ac288b625a2f93592ef50649bcd8c4
armanruet <armanruet@gmail.com>
2026-05-04 21:18:14 +0000

    Task 1 added

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """a class Square that defines a square """
+
+    def __init__(self, size):
+        """Defining the menthod for size"""
+        self.size = size
