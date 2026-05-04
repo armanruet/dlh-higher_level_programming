1bad29330586be22726d0a8198f2b39a765dfdb3
armanruet <armanruet@gmail.com>
2026-05-04 21:10:49 +0000

    added documentation for module,  all Classes and function

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a Square class."""
+
+
 class Square:
     """An empty class Square"""
+
     def __init__(self):
+        """Initialize instances of Class"""
         pass
