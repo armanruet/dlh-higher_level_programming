553677800ed00d85482e6921b1a5c11c8eb63519
armanruet <armanruet@gmail.com>
2026-05-04 20:59:26 +0000

    Task 0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-Class Square():
-	def __init__(selfY):
-
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+class Square:
+    def __init__(self):
+        pass
