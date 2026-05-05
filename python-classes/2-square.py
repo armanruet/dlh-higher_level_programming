4227df2ad39f24a09319fe154566e88f2efc050f
armanruet <armanruet@gmail.com>
2026-05-05 06:41:19 +0000

    Task 2 done

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,3 +8,12 @@ class Square:
     def __init__(self, size=0):
         """Defining the menthod for size"""
         self.__size = size
+        try:
+            if not isinstance(self.__size, int):
+                raise TypeError
+            elif self.__size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
