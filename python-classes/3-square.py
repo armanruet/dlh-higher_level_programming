67c6709003983067b8f4af24ec3a7cd6663af29e
armanruet <armanruet@gmail.com>
2026-05-05 07:19:53 +0000

    task 3 area calculation

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Square with size"""
+
+
+class Square:
+    """a class Square that defines a square """
+
+    def __init__(self, size=0):
+        """Defining the menthod for size"""
+	self.__size = size
+        try:
+            if not isinstance(self.__size, int):
+                raise TypeError
+            elif self.__size < 0:
+                raise ValueError
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
+	def area(self):
+		"""Area calculation"""
+		return self.size ** 2
