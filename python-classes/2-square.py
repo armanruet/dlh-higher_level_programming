55b3c599e159e9ca6fd257299879365cbe4dfd7a
armanruet <armanruet@gmail.com>
2026-05-05 06:55:16 +0000

    pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Square with size"""
-
-
 class Square:
     """a class Square that defines a square """
-
     def __init__(self, size=0):
         """Defining the menthod for size"""
         self.__size = size
