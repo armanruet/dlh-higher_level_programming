d66d9b064b2043adcf0a413775ba954c9796d399
armanruet <armanruet@gmail.com>
2026-05-05 06:58:59 +0000

    pycodestyle error fixed

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """Square with size"""
+
+
 class Square:
     """a class Square that defines a square """
+
     def __init__(self, size=0):
         """Defining the menthod for size"""
         self.__size = size
         try:
             if not isinstance(self.__size, int):
-                raise TypeError("size must be an integer")
+                raise TypeError
             elif self.__size < 0:
-                raise ValueError("size must be >= 0")
+                raise ValueError
+        except TypeError:
+            print("size must be an integer")
+        except ValueError:
+            print("size must be >= 0")
