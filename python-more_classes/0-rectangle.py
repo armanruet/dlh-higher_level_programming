57ff4fe243401ed3c507b4de40a0782aafa458f7
armanruet <armanruet@gmail.com>
2026-05-07 12:12:56 +0000

    Task 0 done

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Creating aan empty class Rectangle that defines a rectangle
+"""
+class Rectangle:
+	def __init__(self):
+		pass
+
+my_rectangle = Rectangle()
+print(type(my_rectangle))
+print(my_rectangle.__dict__)
