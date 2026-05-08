89d612b87d4de8f770588df3fd4548039ef53533
armanruet <armanruet@gmail.com>
2026-05-08 23:11:16 +0000

    task 0 README

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, "r") as f:
-        for i in f.readlines():
-            print(i, end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
