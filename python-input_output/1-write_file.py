33eede280cc897fd9cf8382dd38c7f156fc203f0
armanruet <armanruet@gmail.com>
2026-05-09 21:06:54 +0000

    Task 1 done

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Function that writes a string to a text file (UTF8)\
- and returns the number of characters """
+"""Module that writes a string to a text file"""
 
 
 def write_file(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as f:
+    """Writes a string to a text file (UTF8)
+    and returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
         f.write(text)
     return len(text)
