971039c42399a3127cf08542a52b27904bebba12
armanruet <armanruet@gmail.com>
2026-05-10 18:40:21 +0000

    Task o1 done

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""serialize and deserialize custom Python objects """
+import pickle
+
+
+class CustomObject:
+
+    def __init__(self, name: str, age: int, is_student: bool):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Age: {}".format(self.age))
+        print("Is Student: {}".format(self.is_student))
+
+    def serialize(self, filename):
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None
