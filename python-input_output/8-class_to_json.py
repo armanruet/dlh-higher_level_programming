6ae64bcb9f7ec5c65cc8b7044fad19df6e2caa1d
armanruet <armanruet@gmail.com>
2026-05-10 14:07:58 +0000

    Task 8 import removed

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,8 +2,6 @@
 """Write a function that returns the dictionary \
         description with simple data structure (list, dictionary, \
         string, integer and boolean) for JSON serialization of an objec"""
-import json
-
 
 def class_to_json(obj):
     return obj.__dict__
