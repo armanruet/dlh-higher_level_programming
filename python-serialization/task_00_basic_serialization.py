43f25ba4e824bdcdbb61eaa6f9185022301d6631
armanruet <armanruet@gmail.com>
2026-05-10 17:41:21 +0000

    Task OO Serailization and deserialization with file not string

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,9 +5,11 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     # Your code here to serialize and save data to the specified file
-    filename = json.dumps(data)
+    with open(filename, "w") as f:
+        json.dump(data, f)
 
 
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
-    return json.loads(filename)
+    with open(filename, "r") as f:
+        return json.load(f)
