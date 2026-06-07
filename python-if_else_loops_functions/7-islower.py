f59ccdf44d8d5f0f64e3b0c7ce4b6a8f0ec619f7
armanruet <armanruet@gmail.com>
2026-06-07 15:01:50 +0000

    Task 7 done

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""checking for lowercase"""
+
+
+def islower(c):
+    """defining the function"""
+    if c in "abcdefghijklmnopqrstuvwxyz":
+        return True
+    else:
+        return False
